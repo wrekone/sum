8769f7b4307e388337b9260a9f69329fbb2b4f96
wrekone <wrekone@users.noreply.github.com>
2020-01-23 10:53:37 -0800

    define func SumAll

--- sum.go
+++ sum.go
@@ -1,9 +1,15 @@
 package main
 
+// Sum adds all numbers in a given slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll returns a sum of slices
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	return
 }
